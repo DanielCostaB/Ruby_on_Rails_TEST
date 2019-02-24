65a060ab30cf68d08b1e746f45ab3e110a06adf7
Zartdc <daniel.c.dan@gmail.com>
2019-02-24 14:06:27 -0300

    Partials

--- Gemfile
+++ Gemfile
@@ -10,6 +10,10 @@ gem 'sqlite3', '~> 1.3.13'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
+gem 'bootstrap-sass', '~> 3.3.6'
+#gem 'autoprefixer-rails'
+gem 'jquery-rails'
+#gem 'sassc-rails', '>= 2.1.0'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #Definindo as rotas entre as views e controles
+  #Definindo as rotas entre as views e controles:
+  #no caso o controlador "static_pages" e a ação: index
 
   root to: "static_pages#index"
 
