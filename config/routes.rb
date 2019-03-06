d216913d9f9f31a1e664e699eb4da9d769870505
Zartdc <daniel.c.dan@gmail.com>
2019-03-06 11:02:42 -0300

    Controllers validações e Autenticação

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  #Definindo as rotas entre as views e controles:
-  #no caso o controlador "static_pages" e a ação: index
+  get 'sessions/new'
+  #resources :contacts
+ 
 
   root to: "static_pages#index"
 
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
-#get envia a rota relacionada a página criada no caso: "contato e sobre"
-#referencia da rota, " to: 'static_pages#sobre ou contato' "
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+  delete 'sair', to: 'sessions#destroy'
+
+resources :contacts
+resources :users, only: [:new, :create, :show]
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_03_05_213542) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: " ", null: false
+    t.string "name", default: " ", null: false
+    t.string "password_digest", default: " ", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
